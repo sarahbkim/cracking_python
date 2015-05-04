00b575af59538b444e978bc614a7e32207cc839e
sarahbkim <pnkdaegee@gmail.com>
2015-05-03 20:14:50 -0700

    add all paths in a depth first search

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -37,7 +37,7 @@ from collections import deque
 #             else:
 #                 d[bucket] = [word]
     
-#     # add vertices and edges for words in same bucket
+#     #  vertices and edges for words in same bucket
 #     for bucket in d.keys():
 #         for word1 in d[bucket]:
 #             for word2 in d[bucket]:
@@ -78,9 +78,68 @@ def my_bfs(graph, start, end):
                 path.append(start)
                 q.append(item)
 
-print my_bfs(graph, 'A', 'E')
-print my_bfs(graph, 'B', 'D')
-print my_bfs(graph, 'A', 'F')
+
+# print my_bfs(graph, 'A', 'E')
+# print my_bfs(graph, 'B', 'D')
+# print my_bfs(graph, 'A', 'F')
+
+graphB = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
 
 
 
+def my_dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        curr = stack.pop()
+        if curr not in visited:
+            visited.add(curr)
+            stack.extend(set(graph[curr])-visited)
+    return visited
+
+# print my_dfs(graphB, 'A')
+
+# all possible paths for A:
+# [A, B, D]
+# [A, C]
+# [A, B, E, F]
+
+
+def my_dfs_forest(graph, start):
+    q = deque()
+    q.append(start)
+    forest = [] # collection of paths
+    main_visited = set()
+
+    while q:
+        curr = q.pop()
+        if curr not in main_visited:
+            main_visited.add(curr)
+            q.extend(set(graph[curr]-main_visited))
+
+            visited = set()
+            path = []
+            sub_q = deque()
+            sub_q.append(curr)
+
+            while sub_q:
+                sub_curr = sub_q.popleft()
+                path.append(sub_curr)
+                
+                for item in graph[sub_curr]:
+                    if item not in visited:
+                        path.append(item)
+                        visited.add(item)
+                        sub_q.append(item)
+                    if path not in forest:
+                        forest.append(path)
+                path = [] # reset path
+            visited = set() # reset visited
+            
+    return forest
+
+print my_dfs_forest(graphB, 'A')
